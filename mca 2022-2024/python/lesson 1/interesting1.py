65276bf4083a98b3ec76e81af8f344b4da36adc5
Neel Chalke <74174374+Avry123@users.noreply.github.com>
2022-11-09 09:11:46 +0530

    All the structure you want
    Contains all the files now pay mind.

---
+++ mca 2022-2024/python/lesson 1/interesting1.py
@@ -0,0 +1,9 @@
+#Print this
+#lst = [1 ,3, 'hi', 'bye']
+#for i in range(len(lst)):
+    #print(i)
+    #lst[i] = lst[i] * 2
+#print(lst)
+
+for i in range(0,5):
+    print(i)
---
+++ mca 2022-2024/python/lesson 1/lesson1.py
@@ -0,0 +1,9 @@
+
+c = input('Enter the value for c ')
+d = input('Enter the value for d ')
+sum = c + d
+print(sum)
+
+
+
+
