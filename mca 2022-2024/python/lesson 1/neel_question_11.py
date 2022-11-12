a2b6c5382ae4d10b20b70d8e3a21b36c4cc5d956
Neel Chalke <74174374+Avry123@users.noreply.github.com>
2022-11-13 01:00:54 +0530

    question 11
    This is question 11 and an attempt at an answer.

--- mca 2022-2024/python/lesson 1/neel_question_11.py
+++ mca 2022-2024/python/lesson 1/neel_question_11.py
@@ -1,3 +1,42 @@
 # Write a python function ascending(l) that returns.true if each in it's input list is atleast as big as the one before it?
 # ascending([])
 # ascending([3,3,4])
+
+repeatOrNot = input('Start Y/Stop N ')
+
+a = []
+
+
+def ascendingList(m) :
+    iterate2 = 0
+    correctedList = []
+    trueOrFalse = 'true'
+    correctedList += m
+    correctedList.sort()
+    while iterate2 < len(correctedList) :
+        if (a[iterate2] == correctedList[iterate2]) :
+            iterate2 += 1
+        else :
+            trueOrFalse = 'false'
+            iterate2 += 1
+    print(trueOrFalse)
+
+while repeatOrNot.lower() == 'y' :
+    letterOrNot = input('Letter l/Number n ')
+    noOfInputs = input('how many times do you want to add : ')
+    iterate = 0
+    if letterOrNot.lower() == 'l':
+        while iterate < int(noOfInputs) :
+            addingItems = input('Letter: ')
+            a.append(addingItems)
+            iterate += 1
+    else :
+        while iterate < int(noOfInputs) :
+            addingItems = input('Number: ')
+            a.append(int(addingItems))
+            iterate += 1
+    ascendingList(a)
+    a = []
+    repeatOrNot = input('Start Y/Stop N ')
+        
+    
