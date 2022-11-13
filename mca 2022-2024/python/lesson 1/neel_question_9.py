4f48c9b9e7f51c74ba11adef2ede816704f058fd
Neel Chalke <74174374+Avry123@users.noreply.github.com>
2022-11-13 11:17:39 +0530

    It works
    It works now to test it.

--- mca 2022-2024/python/lesson 1/neel_question_9.py
+++ mca 2022-2024/python/lesson 1/neel_question_9.py
@@ -10,3 +10,28 @@
 
 # wellbracketed"(a+b)(a+b)"
 # true
+
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+
+def correctParenthesis(a) :
+    num1 = 0
+    num2 = 0
+    for i in [*a] :
+        if i == '(' :
+            num1 += 1
+        if i == ')' :
+            num2 += 1
+    if num1 != num2:
+       print('Sorry, there is an error in paranthesis') 
+    else :
+       print('The following thing is working')
+
+
+repeatOrNot = input('START Y/STOP N')
+
+while repeatOrNot.lower() == 'y' :
+    stringValue = input('Please give a string')
+    correctParenthesis(stringValue)
+    repeatOrNot = input('START Y/STOP N')
+    
