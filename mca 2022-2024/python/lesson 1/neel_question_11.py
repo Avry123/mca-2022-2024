2a05ad53ad2a7c96fcc5bf62adb86962e4746d52
Neel Chalke <74174374+Avry123@users.noreply.github.com>
2022-11-15 10:54:08 +0530

    11 question updated to bubble Sort.
    Used bubble sort instead of the sort() built in function.

--- mca 2022-2024/python/lesson 1/neel_question_11.py
+++ mca 2022-2024/python/lesson 1/neel_question_11.py
@@ -2,18 +2,24 @@
 # ascending([])
 # ascending([3,3,4])
 
+# Logic 
+# Create a sorted list called correctedList and one which is not sorted we call it as a
+# Now we compare the correctedList to a one by one and if they are equal we add iterate 3 by 1
+# If they are in ascending order throught out the iterate3  should be equal to the lenght of the correctedList
+
 repeatOrNot = input('Start Y/Stop N ')
 
 a = []
 
-
 def ascendingList(m) :
     iterate2 = 0
     iterate3 = 0
+    check10 = 0
     correctedList = []
     trueOrFalse = 'true'
     correctedList += m
-    correctedList.sort()
+    # correctedList.sort()
+    correctedList = bubbleSort(correctedList)
     while iterate2 < len(correctedList) :
         if (m[iterate2] == correctedList[iterate2]) :
             iterate3 += 1
@@ -25,6 +31,19 @@ def ascendingList(m) :
     iterate2 = 0
     iterate3 = 0
     correctedList = []
+    
+def bubbleSort(param) :
+    iterate0 = 0
+    num2 = len(param)
+    num1 = len(param) - 1
+    for i in range(num2):
+        for j in range(i):
+            if (param[j] > param[i]) :
+                b = param[i]
+                param[i] = param[j]
+                param[j] = b
+    return param
+
 
 while repeatOrNot.lower() == 'y' :
     letterOrNot = input('Letter l/Number n ')
