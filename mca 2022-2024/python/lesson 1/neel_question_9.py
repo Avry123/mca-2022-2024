67a7f029b7d93eba10a15ff0252952bd232893c1
Neel Chalke <74174374+Avry123@users.noreply.github.com>
2022-11-15 12:38:55 +0530

    The new thing was added to fixed the error.
    It works and not it is great. 9th question won.

--- mca 2022-2024/python/lesson 1/neel_question_9.py
+++ mca 2022-2024/python/lesson 1/neel_question_9.py
@@ -14,18 +14,32 @@
 # Online Python compiler (interpreter) to run Python online.
 # Write Python 3 code in this online editor and run it.
 
+# If you find ( add num1 by 1
+# If you find ) add num2 by 1
+# Then compare num1 and num2 , if num1 and num2 are equal then the next step happens
+# If num2 gets incremented before num1, then turn parenthisisError to true, if parenthisisError is true then there is an error.
+
 def correctParenthesis(a) :
     num1 = 0
     num2 = 0
+    parenthisisError = 'false' 
     for i in [*a] :
         if i == '(' :
             num1 += 1
+            # print('This is num1 ', num1 , '\n')
         if i == ')' :
             num2 += 1
+            # print('This is num2 ', num2 , '\n')
+            if (num2 > num1) :
+                parenthisisError =  'true'
     if num1 != num2:
        print('Sorry, there is an error in paranthesis') 
     else :
-       print('The following thing is working')
+           if (parenthisisError == 'true') :
+               print('There is an error')
+           else :
+                print('This Works')
+       
 
 
 repeatOrNot = input('START Y/STOP N')
