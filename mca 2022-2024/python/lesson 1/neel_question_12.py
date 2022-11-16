51c104dfd31dab0fa3e953a665339c9684396b2a
Neel Chalke <74174374+Avry123@users.noreply.github.com>
2022-11-16 20:54:24 +0530

    Things with question 12 taken care of
    16/11/22

--- mca 2022-2024/python/lesson 1/neel_question_12.py
+++ mca 2022-2024/python/lesson 1/neel_question_12.py
@@ -1,71 +1,48 @@
 # Online Python compiler (interpreter) to run Python online.
 # Write Python 3 code in this online editor and run it.
-repeatOrNot = input('Start Y/Stop N')
+
+runORStop = input('Start Y/Stop N')
 
 initialLetter = []
 
-def ascending(m) :
+def showDict(m) :
+    # print(m)
+    # Collecting the string
     iterate0 = 0
-    name = 'neel'
-    while iterate0 < len(m) :
+    # Collecting the initial letters while also avoiding repetition.
+    while iterate0 < len(m):
         if (m[iterate0][0].lower() not in initialLetter) :
             initialLetter.append(m[iterate0][0].lower())
         iterate0 += 1
-    iterate1 = 0
     initialLetter.sort()
-    while iterate1 < len(initialLetter) :
-        name = initialLetter[iterate1]
-        dict ={name : []}
-        print(dict)
-        if (name) :
-            iterate2 = 0
-            while iterate2 < len(m) :
-                if ()
-                iterate2 += 1
-        iterate1 += 1
-        
-
-while repeatOrNot.lower() == 'y':
-    answer = input('Give us a string')
-    splitString = answer.split()
-    ascending(splitString)
-    repeatOrNot = input('Start Y/Stop N')
-    
-    
-    
-    # Online Python compiler (interpreter) to run Python online.
-# Write Python 3 code in this online editor and run it.
-string10 = 'HEY HOW ARE YOU DOING OOO'
-splitList = string10.split()
-
-a = ['h', 'a', 'y', 'd' , 'o'] 
-# Initial Letter
-
-name = 'neel'
-
-# dict = {name : []}
-
-# dict['neel'].append('hello')
-
-iterate0 = 0
-
-while iterate0 < len(a) :
+    name = 'neel'
     iterate1 = 0
-    a.sort()
-    name = a[iterate0]
-    dict = {name : []}
-    if (name) :
-       while iterate1 < len(splitList) :
-           if (name == splitList[iterate1][0].lower()) :
-               dict[name].append(splitList[iterate1])
-           iterate1 += 1
-    print(dict)
-    iterate0 += 1
-
-
+    # Looping Initialletter
+    while iterate1 < len(initialLetter):
+        name = initialLetter[iterate1]
+        dict = {name : []}
+        # print(dict)
+        iterate2 = 0
+        # Looping the list and if same appending that item to the array
+        while iterate2 < len(m) :
+            if (name == m[iterate2][0].lower()) :
+                 dict[name].append(m[iterate2])
+                #  This loop does six times so that why the wierd answer
+            # print(dict)
+            iterate2 += 1
+            # This works 4 times.
+        print(dict)
+        iterate1 += 1
     
     
+        
+            
 
-
-
-
+while runORStop.lower() == 'y':
+    answer = input('Give a string ')
+    splitList = answer.split()
+    showDict(splitList)
+    initialLetter = []
+    runORStop = input('Start Y/Stop N')
+    
+    
