cb0eda13b76a0062b0911e6efd160f50c495d045
Neel Chalke <74174374+Avry123@users.noreply.github.com>
2022-11-16 18:11:14 +0530

    thins added
    add

--- mca 2022-2024/python/lesson 1/neel_question_12.py
+++ mca 2022-2024/python/lesson 1/neel_question_12.py
@@ -1,148 +1,71 @@
-# Write a python program to create a dictionary with key as first character and value as words starting with that character.
-# The program take a string and create a dictionary with key as first character and value as words starting wth that character.
-# example: enter string: Python is my favorite programming language in the entire world 
-# ('e',':',['entire'])
-# ('f',':',['favorite'])
-# ('i',':', ['is', 'in'])
-# ('m', ':', ['most', 'my'])
-# ('l', ':', ['language'])
-# ('p', ':', ['programming' , 'python'])
-# ..
-
-# a = 'name'
-
-# thingTODo = {a : 'neel'}
-
-# print(thingTODo)
-
-# a = ['neel','reet','mangesh']
-
-# b = a[0]
-
-# print(b[0:1])
-
-listGettingInputs = []
-
-repeatOrNot = input('Start Y/Stop N')
-
-def dictionary10(m) :
-    firstItem = m[0]
-    firstLetterOfFirstItem = firstItem[0:1]
-    dictionaryToPrint = {firstLetterOfFirstItem : listGettingInputs}
-    print(dictionaryToPrint)
-
-while repeatOrNot.lower() == 'y' :
-    b = input('No of inputs you want in a list: ')
-    
-    
-    
-    
-    
-    # Online Python compiler (interpreter) to run Python online.
+# Online Python compiler (interpreter) to run Python online.
 # Write Python 3 code in this online editor and run it.
-repeatOrNot = input('Start Y/Stop N ')
-
-stringBrokenIntoList = []
+repeatOrNot = input('Start Y/Stop N')
 
 initialLetter = []
 
-elementWithInitialLetter = []
-
-def arrange(m) :
-    # Getting rid of , ? < !
-    for i in m :
-        if i not in initialLetter :
-            initialLetter.append(i)
-    # Loop for initialLetter
+def ascending(m) :
     iterate0 = 0
-    while iterate0 < len(initialLetter) :
-        iterate1 = 0
-        while iterate1 < len(stringBrokenIntoList) :
-            print('This is second loop', stringBrokenIntoList[iterate1])
-            iterate1 += 1
-        print('This is first loop', initialLetter[iterate0])
+    name = 'neel'
+    while iterate0 < len(m) :
+        if (m[iterate0][0].lower() not in initialLetter) :
+            initialLetter.append(m[iterate0][0].lower())
         iterate0 += 1
+    iterate1 = 0
+    initialLetter.sort()
+    while iterate1 < len(initialLetter) :
+        name = initialLetter[iterate1]
+        dict ={name : []}
+        print(dict)
+        if (name) :
+            iterate2 = 0
+            while iterate2 < len(m) :
+                if ()
+                iterate2 += 1
+        iterate1 += 1
+        
 
-while repeatOrNot.lower() == 'y' :
-    stringCollection = input('Please give string ')
-    stringCollection.split()
-    stringBrokenIntoList += stringCollection
-    arrange(stringBrokenIntoList)
-    repeatOrNot = input('Start Y/Stop N ')
-    
-    
-    
-    
-    
-    
-    
+while repeatOrNot.lower() == 'y':
+    answer = input('Give us a string')
+    splitString = answer.split()
+    ascending(splitString)
+    repeatOrNot = input('Start Y/Stop N')
     
     
     
     # Online Python compiler (interpreter) to run Python online.
 # Write Python 3 code in this online editor and run it.
+string10 = 'HEY HOW ARE YOU DOING OOO'
+splitList = string10.split()
+
+a = ['h', 'a', 'y', 'd' , 'o'] 
+# Initial Letter
+
+name = 'neel'
+
+# dict = {name : []}
+
+# dict['neel'].append('hello')
+
+iterate0 = 0
+
+while iterate0 < len(a) :
+    iterate1 = 0
+    a.sort()
+    name = a[iterate0]
+    dict = {name : []}
+    if (name) :
+       while iterate1 < len(splitList) :
+           if (name == splitList[iterate1][0].lower()) :
+               dict[name].append(splitList[iterate1])
+           iterate1 += 1
+    print(dict)
+    iterate0 += 1
 
 
-# 1. Following takes the first letter from the list and avoids repetition and appends it to the new list.
-
-# a = ['neel', 'reet', 'mangesh', 'tanvi' , 'tanvi']
-
-# commonLetter = []
-
-# i = 0
-
-# while i < len(a) :
-#     if (a[i][0]  not in commonLetter) :
-#         commonLetter.append(a[i][0])
-#     i += 1
-
-# print(commonLetter)
-
-a = ['neel', 'reet', 'mangesh', 'tanvi' , 'tanvi' , 'nimesh']
-
-b = ['n' , 'r', 'm', 't']
-
-addingItems = []
-
-# b will be the while loop here, inside of this there will be the a loop
-if (b[0] == a[0][0]) :
-    addingItems.append(a[0])
-    # iterate += 1 
-print(addingItems)
-
-if (b[0] == a[1][0]) :
-    addingItems.append(a[1])
-    # iterate += 1 
-print(addingItems)
-
-if (b[0] == a[2][0]) :
-    addingItems.append(a[2])
-    # iterate += 1 
-print(addingItems)
     
-if (b[0] == a[3][0]) :
-    addingItems.append(a[3])
-    # iterate += 1 
-print(addingItems)
-
-if (b[0] == a[4][0]) :
-    addingItems.append(a[4])
-    # iterate += 1 
-print(addingItems)
-
-if (b[0] == a[5][0]) :
-    addingItems.append(a[5])
-    # iterate += 1 
-print(addingItems)
-
-# iterate0 += 1
-    iterate0 = 0
-    while iterate0 < int(b) :
-        items = input('Item: ')
-        listGettingInputs.append(items)
-        iterate0 += 1
-    dictionary10(listGettingInputs)
-    listGettingInputs = []
-    repeatOrNot = input('Start Y/Stop N')
-        
     
+
+
+
+
